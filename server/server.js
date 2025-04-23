b6214efaa2b5659d620fe484a5562c9f7ab01b7e
Amritha <amritharnair2@gmail.com>
2025-04-24 02:00:04 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieparser())
 app.use(cors({
-        origin: "https://myapp-a6kt.vercel.app",
+        origin: 'http://localhost:5173',
         credentials: true
 }))
 dbConnection()
